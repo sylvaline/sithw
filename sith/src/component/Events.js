9be5be52ca4171bdfeffadc2718f2a1d139816b9
sylvaline <okekesylvaline80@gmail.com>
2021-06-30 12:56:15 +0100

    first commit

--- sith/src/component/Events.js
+++ sith/src/component/Events.js
@@ -4,6 +4,7 @@ function Events() {
     return (
         <div className="container">
             <h2>This is Events page</h2>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam itaque, magnam, quam quod, cupiditate fugiat ullam dicta mollitia delectus vitae perferendis vero maiores eaque dolore fuga numquam rerum! Doloremque, sequi!</p>
         </div>
     )
 }
